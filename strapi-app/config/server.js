9b96a49e0bf26192e8aa8933cde37192036729cc
santhosh3670 <santhoshkumar.sathy2216@gmail.com>
2025-07-17 18:59:15 +0530

    Initial commit: Strapi project with Docker setup

---
+++ strapi-app/config/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: (process.env.APP_KEYS || '').split(','),
+  },
+});
+
