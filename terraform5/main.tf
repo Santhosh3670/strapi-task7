572aabcb87ddb1504bd1fcd623cf8c1769a96719
santhosh3670 <santhoshkumar.sathy2216@gmail.com>
2025-07-22 11:43:11 +0530

    Add Task 5: Terraform deploy with dynamic image tag

---
+++ terraform5/main.tf
@@ -0,0 +1,65 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "strapi_sg" {
+  name        = "strapi_sg_task5"
+  description = "Allow Strapi ports"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "strapi_ec2" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.strapi_sg.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install docker.io -y
+              systemctl start docker
+              systemctl enable docker
+              docker pull santhosh3016/strapi-app:${var.image_tag}
+              docker run -d -p 80:1337 santhosh3016/strapi-app:${var.image_tag}
+            EOF
+
+  tags = {
+    Name = "StrapiEC2-Task5"
+  }
+}
