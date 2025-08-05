bdbdf6d91ca01b35943c155811f6669c0c110a39
santhosh3670 <santhoshkumar.sathy2216@gmail.com>
2025-08-05 13:39:29 +0530

    task11 redeploy

--- terraform12/logs.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_cloudwatch_log_group" "strapi_log_group" {
-  name              = "/ecs/strapi-sk"
-  retention_in_days = 7
-
-  tags = {
-    Name = "strapi-log-group-sk"
-  }
-}
-
--- terraform12/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "alb_dns_name" {
-  description = "Public ALB URL to access Strapi"
-  value       = aws_lb.strapi_alb.dns_name
-}
-output "log_group_name" {
-  description = "CloudWatch Log Group for ECS logs"
-  value       = aws_cloudwatch_log_group.strapi_log_group.name
-}
-
